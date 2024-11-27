69cb34b869435e8f401518ebc3f16803aecc5a5c
chancel.yang <ycs1026@vip.qq.com>
2024-11-27 17:39:38 +0800

    Fix /static/** trigger router "/<memo_id>" Add memo class

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, g, request, render_template
 from flask_compress import Compress
-from flaskr.dependents import cache, config, CONST_VERSION
+from flaskr.dependents import config, CONST_VERSION, Memo
 from flaskr.utils import build_random_string, json_response, build_content_qrcode
 import base64
 
@@ -9,12 +9,15 @@ app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
 Compress(app)
 
+# Memo initialization
+memo = Memo(disk_cache_dir=config["system"]["cache"], expire=config["memo"]["expire"])
+
 
 @app.route("/", methods=["GET"])
 def index():
     verify_code = build_random_string(length=int(config["memo"]["id_defalt_length"]))
     while True:
-        if not cache.get(verify_code):
+        if not memo.get(verify_code):
             break
         verify_code = build_random_string(length=(config["memo"]["id_defalt_length"]))
     return render_template("template_index.html", verify_code=verify_code)
@@ -24,15 +27,9 @@ def index():
 def get_memo(memo_id: str):
     if memo_id == "help":
         return render_template("template_help.html", max_size=config["memo"]["max_size"], expire=config["memo"]["expire"], version=CONST_VERSION)
-    if not cache.get(memo_id.upper()):
-        if config["memo"]["expire"] == 0:
-            cache.set(key=memo_id.upper(), value="")
-        if config["memo"]["expire"] != 0:
-            cache.set(key=memo_id.upper(), value="", expire=config["memo"]["expire"])
-
     qrcode_base64 = "data:image/png;base64,%s" % build_content_qrcode(content=request.base_url)
     hex_str = base64.b16encode(memo_id.encode()).decode()
-    return render_template("template_memo.html", memo_id=memo_id, localStoreLength=10, img=qrcode_base64, memo_content=cache.get(memo_id.upper()), hex_str=hex_str)
+    return render_template("template_memo.html", memo_id=memo_id, localStoreLength=10, img=qrcode_base64, memo_content=memo.get(memo_id.upper()), hex_str=hex_str)
 
 
 @app.route("/immutable/<hex_str>", methods=["GET"])
@@ -42,9 +39,9 @@ def get_immutable(hex_str: str):
         memo_id = base64.b16decode(hex_str.encode()).decode("utf8")
     except Exception:
         return json_response(success=False, message="hex_str args illegal")
-    if not cache.get(memo_id.upper()):
+    if not memo.get(memo_id.upper()):
         return json_response(success=False, message="memo not found")
-    return render_template("template_immutable.html", html=cache.get(memo_id.upper()))
+    return render_template("template_immutable.html", html=memo.get(memo_id.upper()))
 
 
 @app.route("/rest/api/v1/memo", methods=["POST"])
@@ -54,10 +51,7 @@ def post_memo():
     size = len(content) / 1024 / 1024
     if size > int(config["memo"]["max_size"]):
         return json_response(success=False, message="Memo exceed max size(%smb)" % config["memo"]["max_size"])
-    if config["memo"]["expire"] == 0:
-        cache.set(key=memo_id, value=content)
-    if config["memo"]["expire"] != 0:
-        cache.set(key=memo_id, value=content, expire=config["memo"]["expire"])
+    memo.set(key=memo_id, value=content)
     return json_response(success=True, message="save success")
 
 
--- flaskr/dependents.py
+++ flaskr/dependents.py
@@ -2,6 +2,9 @@ import os
 
 from yaml import safe_load
 from diskcache import Cache
+import threading
+
+CONST_VERSION = "V2.0"
 
 
 def load_yaml(file_path):
@@ -11,10 +14,6 @@ def load_yaml(file_path):
 
 config = load_yaml(os.environ["CONFIG"])
 
-cache = Cache(config["system"]["cache"])
-
-CONST_VERSION = "V2.0"
-
 
 class Response:
     def __init__(self, status: int, msg: str, data: dict = {}):
@@ -23,3 +22,39 @@ class Response:
         self.msg = msg
         self.data = data
         self.version = config["system"]["version"]
+
+
+class Memo:
+    _instance = None
+    _lock = threading.Lock()
+
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(Memo, cls).__new__(cls)
+                    cls._instance._initialized = False
+        return cls._instance
+
+    def __init__(self, disk_cache_dir, expire=0):
+        if self._initialized:
+            return
+        self.disk_cache = Cache(disk_cache_dir)
+        self.expire = expire
+        self._initialized = True
+        self.flask_cache = None
+
+    def set(self, key, value):
+        if self.expire != 0:
+            self.disk_cache.set(key, value, expire=self.expire)
+        if self.expire == 0:
+            self.disk_cache.set(key, value)
+
+    def get(self, key):
+        value = self.disk_cache.get(key)
+        if value is not None:
+            return value
+
+        # If not found in both, initialize with empty string in DiskCache
+        self.disk_cache.set(key, "", expire=self.expire)
+        return ""
